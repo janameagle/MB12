a5158bf451932d4c4006276c9f326864f2f56a38
janameagle <jana.maier@mail.de>
2020-10-30 12:19:25 +0100

    something changed

--- TestFile_J.R
+++ TestFile_J.R
@@ -1,1 +1,11 @@
 #This is a test file
+
+install.packages("raster")
+library(raster)
+install.packages("rgdal")
+library(rgdal)
+
+germany <- readOGR("C:/Users/jmaie/OneDrive/R")
+plot(germany)
+setwd("C:/Users/jmaie/OneDrive/R")
+prec <- getData("worldclim" , var="prec" , res=10)
